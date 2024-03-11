2650479e69f1637db72991fca5757591e1a9d110
infolanze <infolanze.tech@gmail.com>
2024-03-11 12:59:01 +0530

    First

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -37,7 +37,7 @@ function Header() {
     <div className={isSticky ? 'Header-p1 Sticky-p1' : 'Header-p1'}>
     <div className="shadow-xl w-full bg-[#25aae1] py-3">
       <div className="lg:px-16 md:px-7 px-5 lg:flex justify-between items-center">
-        <div className="">
+        <div className="z-40">
           <span>
             <img className="w-48" src={Logo} alt="nishant" />
           </span>
@@ -45,16 +45,16 @@ function Header() {
 
         <div
           onClick={() => setIsOpen(!isOpen)}
-          className="lg:hidden absolute md:right-8 md:top-6 right-5 top-6 cursor-pointer h-fit z-50"
+          className="lg:hidden absolute md:right-8 md:top-6 right-5 top-6 cursor-pointer h-fit z-40"
         >
           {isOpen ? <FaBars className="text-3xl" /> : <AiOutlineClose className="text-3xl" />}
         </div>
 
         <nav
-          className={`nav-menu lg:flex lg:pb-0 lg:py-0 md:py-7 py-7 lg:items-center text-base absolute lg:static bg-[#25aae1] lg:z-auto z-50 right-0 w-full lg:w-auto md:pl-0 transition-all duration-500 ease-in 
+          className={`nav-menu lg:flex lg:pb-0 lg:py-0 md:py-7 py-7 lg:items-center text-base absolute lg:static bg-[#25aae1] lg:z-auto z-30 right-0 w-full lg:w-auto md:pl-0 transition-all duration-500 ease-in 
           ${isOpen ? "top-[-600px]" : "top-[65px]"}` }
         >
-          <div className="bg-[#25aae1] lg:flex gap-1 text-start lg:ps-0 md:ps-5 ps-3 font-family">
+          <div className="bg-[#25aae1] lg:flex gap-1 text-start lg:ps-0 md:ps-5 ps-3 font-family z-40">
             <div className="lg:p-0 p-1.5">
               <NavLink
                 to="/"
