c50003eab1e1b4930a27234b254c9d0e4946c98b
infolanze <infolanze.tech@gmail.com>
2024-04-12 12:51:26 +0530

    Three

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import AboutSection from '../Components/About/AboutSection'
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <AboutSection />
+    </div>
   )
 }
 
