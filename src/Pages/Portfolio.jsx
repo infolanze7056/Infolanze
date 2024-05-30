bed2069b97fadf628b4d1b6370f4fca87b48d5ad
infolanze807 <techteam.infolanze@gmail.com>
2024-05-30 17:41:52 +0530

    helmet changes

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import HeroPortfolio from '../Components/Portfolio/HeroPortfolio/HeroPortfolio';
 import DevelopmentsProject from '../Components/Portfolio/DevelopmentCard/DevelopmentsProject';
 import HeroSection from '../Components/Portfolio/HeroPortfolio/HeroSection';
+import { Helmet } from 'react-helmet';
 
  
 
 const Portfolio = () => {
   return (
     <div className="font-family">
+      <Helmet>
+        <title>Portfolio | Infolanze</title>
+      </Helmet>
       {/* <HeroSection/> */}
       <HeroPortfolio />
       <DevelopmentsProject/>
