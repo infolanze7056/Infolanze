22935a3fc6c015151074a16e94374f350ee2a880
infolanze807 <techteam.infolanze@gmail.com>
2025-12-23 16:11:40 +0530

    updated code

--- src/Components/Contact/ContactSection.jsx
+++ src/Components/Contact/ContactSection.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 // import emailjs from "emailjs-com";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
